cd003a87bb4dc5e9278dcbab62884e4c0b91348c
asdoc <asdocode@gmail.com>
2015-02-06 21:11:07 +0530

    Added tests for Led_Strip_Library

---
+++ Led_Strip_Library/tests/test.py
@@ -0,0 +1,30 @@
+import unittest
+import sys
+sys.path.append("../")
+from ledStrip import ledstrip
+
+class TestLEDStrip(unittest.TestCase):
+
+	##
+	# setUp			Sets the initial values for number of leds and spidev file path.
+	def setUp(self):
+		self.numberOfPixels = 32
+		self.spidevFile = "/dev/spidev0.0"
+	
+	##
+	# testSpi		Tests if the SPI connection is working.
+	def testSpi(self):
+		spidev = file(self.spidevFile, "wb")
+		leds = ledstrip.LEDStrip(self.numberOfPixels,spidev)
+		self.assertTrue(leds.spi,"SPI interface could not be opened")
+
+	##
+	# testNumPixels	Tests if the number of pixels is set properly.
+	def testNumPixels(self):
+		spidev = file(self.spidevFile, "wb")
+		leds = ledstrip.LEDStrip(self.numberOfPixels,spidev)
+		self.assertTrue(leds.spi,"SPI interface could not be opened")
+		self.assertTrue(self.numberOfPixels==leds.numPixels(),"Number of pixels do not match")
+
+if __name__ == "__main__":
+	unittest.main()
